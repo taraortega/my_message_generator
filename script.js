3b0af2f8b0e090d8940dfd8b6241152b138e316c
taraortega <58346310+taraortega@users.noreply.github.com>
2021-04-01 14:56:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-var a = 'Node REPL is fun!';
+const generateRandomNumber = num => {
+    return Math.floor(Math.random() * num)
+};
+
+const personalMessage = {
+    affirmation: ['enough', 'beautiful', 'strong', 'resilient'],
+    emotion: ['to cry', 'not to be okay', 'to be sad', 'to not have it all figured out'],
+    motivation: ['trust God', 'never give up', 'stay strong', 'keep going']
+};
+
+let inspirationalMessage = [];
+
+for (let arr in personalMessage) {
+    let personal = generateRandomNumber(personalMessage[arr].length)
+    switch (arr) {
+        case 'affirmation':
+            inspirationalMessage.push(`You are ${personalMessage[arr][personal]}.`)
+            break
+        case 'emotion':
+            inspirationalMessage.push(`It's okay ${personalMessage[arr][personal]}.`)
+            break
+        case 'motivation':
+            inspirationalMessage.push(`Keep calm and ${personalMessage[arr][personal]}.`)
+            break
+        default:
+            inspirationalMessage.push('Keep going!')
+    }
+};
+
+const formatMessage = inspiration => {
+    const formatted = inspirationalMessage.join('\n')
+    console.log(formatted)
+};
+
+formatMessage(inspirationalMessage);
+
